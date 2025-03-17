790da0348ae62ff5171ba158ed423b9fc45b5950
ZWaretsckG <141160234+ZWaretsckG@users.noreply.github.com>
2025-03-17 10:37:36 -0300

    .

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener(DOMContentLoaded,function()
+ const aumentaFonteBotao = document.getElementById("aumenta-fonte")
+ const aumentaFonteBotao = document.getElementById("diminuir-fonte")
+
+
+)
